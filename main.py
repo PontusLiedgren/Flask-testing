44d6868384909fb091c7a353c23bd11db8bc0226
Pontus Liedgren <pop2strong4u@gmail.com>
2020-01-09 11:48:50 +0100

    Getting started with flask and HTML

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+items = []
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html", items=items)
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    items.append(request.form["item"])
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
