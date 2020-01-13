a8450a6629f754aaa2f508167f4db6cae02b903d
Pontus Liedgren <pop2strong4u@gmail.com>
2020-01-13 10:04:19 +0100

    Start of highscore system

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-items = []
+highscores = []
+
+stats = {
+    "highscores":highscore 
+    "names":name
+    "dates":date
+        }
 
 @app.route("/")
 def index():
-    return render_template("index.html", items=items)
+    return render_template("index.html", highscores=highscores)
 
 @app.route('/submit', methods=["POST"])
 def submit():
-    items.append(request.form["item"])
+    highscores.append(request.form["highscore"])
     return redirect('/')
 
 if __name__ == "__main__":
