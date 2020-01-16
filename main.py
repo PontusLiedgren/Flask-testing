07ddcf1b41579dac779096a63f45cfaac89c52a1
Pontus Liedgren <pop2strong4u@gmail.com>
2020-01-16 11:43:54 +0100

    Sorting highscore list

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
+import operator
 
 app = Flask(__name__)
 
 highscores = []
 
-stats = {
-    "highscores":highscore 
-    "names":name
-    "dates":date
-        }
 
 @app.route("/")
 def index():
-    return render_template("index.html", highscores=highscores)
+    return render_template("index.html", highscores=sorted(highscores, key=lambda i: i['score'], reverse=True))
 
 @app.route('/submit', methods=["POST"])
 def submit():
-    highscores.append(request.form["highscore"])
-    return redirect('/')
+    highscores.append({
+        "score": int(request.form["score"]),
+        "name": request.form["name"],
+        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+
+    return "success", 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
